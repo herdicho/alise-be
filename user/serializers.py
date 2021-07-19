075ee382c07d494a7d7605fecccbe56a21b573d4
herdicho <farras@alterra.id>
2021-07-19 20:16:48 +0700

    renew register code & add validation

--- user/serializers.py
+++ user/serializers.py
@@ -3,7 +3,31 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-
+    password2 = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['username', 'password', 'password2', 'first_name', 'last_name', 'email']
+        extra_kwargs = {
+            'password' : {'write_only' : True}
+        }
+
+    def validate(self, data):
+
+        ''' check if password less than 8 character '''
+        if len(data['password']) < 8:
+            raise serializers.ValidationError({
+                'message' : 'Password must greater than 8 character'
+            })
+
+        ''' check if password & confirm password not match '''
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({
+                'message' : 'Password must be match'
+            })
+
+        ''' delete password2 field from serializer data '''
+        data.pop('password2', None)
+
+        return data
+
--- user/views.py
+++ user/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import User
 from user.serializers import UserSerializer
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-
-class UserRegister(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserRegister(APIView):
+    
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
